16294e4c6eb67b3b926d1c874ee7cc56e863b93b
fdacostamillos <fdacostamillos@atanasoff16.danielcastelao.org>
2016-12-12 09:46:30 +0100

    Cambiar Nombres Variables y Refactorizar

---
+++ src/examenrefactorizar2/ExamenRefactorizar2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenrefactorizar2;
+
+/**
+ *
+ * @author fdacostamillos
+ */
+public class ExamenRefactorizar2 {
+
+
+    public static void Calcular (){
+       int j;
+       int f;
+        j= 8;
+                int i;
+        if (j == 0)
+     f = 1;
+        else
+        {
+     f = 1;
+     for (i = j; i >= 1; i--)
+     {
+         f = f * i;
+     }
+     
+        }    
+
+ 
+
+       System.out.println(f);
+        
+    }
+    public static void main(String[] args) {
+  
+        Calcular();
+
+
+
+
+
+    }
+    }
+    
+
